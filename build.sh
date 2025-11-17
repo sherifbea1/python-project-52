eab7ab84eca381b89da6afa6d10236f23f850438
sherifbea1 <sherifbea1@gmail.com>
2025-11-17 21:24:17 +0300

    fdeffe

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-# Устанавливаем uv (ускоренный менеджер пакетов)
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# Устанавливаем зависимости проекта
 pip install -r requirements.txt
 
-# Применяем миграции и собираем статику
 python manage.py collectstatic --noinput
 python manage.py migrate
