2df8dfbbf75bf50442335e9be67d47ddbce47ce4
sherifbea1 <sherifbea1@gmail.com>
2025-11-18 11:21:51 +0300

    UpdTry5

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import os
+import sys
 import django
 
+sys.path.insert(0, os.path.dirname(__file__))
+
 def pytest_configure():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hexlet_code.settings')
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'hexlet_code.settings'
     django.setup()
