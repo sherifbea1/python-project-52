26421cdbf9f788d73015558ca2261814358b06f9
sherifbea1 <sherifbea1@gmail.com>
2025-12-22 16:51:56 +0300

    Upd22

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
