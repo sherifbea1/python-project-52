dc0d1e8c0b2076a82d9b129d571216434719afa0
sherifbea1 <sherifbea1@gmail.com>
2025-12-26 13:01:17 +0300

    Upd41

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -6,13 +6,21 @@ from django.contrib.auth.forms import UserCreationForm
 class UserCreateForm(UserCreationForm):
     first_name = forms.CharField(label="Имя")
     last_name = forms.CharField(label="Фамилия")
+    password1 = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput
+    )
 
     class Meta:
         model = User
         fields = (
-            "username",
             "first_name",
             "last_name",
+            "username",
             "password1",
             "password2",
         )
