64d0f9d4e998c3964b7f6693800656fa4beb3fa0
sherifbea1 <sherifbea1@gmail.com>
2025-12-28 14:32:05 +0300

    Upd53

--- task_manager/views.py
+++ task_manager/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth import logout
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
